5dff0946a38905701b0479379e99c451768fab73
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-11-22 18:56:01 +0400

    feature: tableView/collectionView cell reuseIdentifier extension auto generator

---
+++ InstsagramKiller/InstsagramKiller/Resources/Extensions/CellReuseIdentifierExtension.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCellReuseIdentifierExtension.swift
+//  InstsagramKiller
+//
+//  Created by Cotne Chubinidze on 22.11.24.
+//
+import UIKit
+
+extension UICollectionViewCell: ReuseIdentifying {}
+extension UITableViewCell: ReuseIdentifying {}
+
+
