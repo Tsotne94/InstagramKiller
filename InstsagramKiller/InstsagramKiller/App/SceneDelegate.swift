66ed56332b610cc54355984ebc9c28915cadde00
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-11-22 14:55:50 +0400

    chore: add emthy viewModel filders

--- InstsagramKiller/InstsagramKiller/App/SceneDelegate.swift
+++ InstsagramKiller/InstsagramKiller/App/SceneDelegate.swift
@@ -19,7 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         tabBarAppearance.backgroundColor = UIColor(named: BackgroundColors.accent.rawValue)
         UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
         UITabBar.appearance().standardAppearance = tabBarAppearance
-        
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
