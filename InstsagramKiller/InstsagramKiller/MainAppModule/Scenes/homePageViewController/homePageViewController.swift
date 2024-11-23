990603f7a3c8f79ec1dd16880ca016c71b9204b2
BekaL <beka75610@gmail.com>
2024-11-23 11:15:19 +0400

    Initial commit

--- InstsagramKiller/InstsagramKiller/MainAppModule/Scenes/homePageViewController/homePageViewController.swift
+++ InstsagramKiller/InstsagramKiller/MainAppModule/Scenes/homePageViewController/homePageViewController.swift
@@ -12,7 +12,7 @@ class homePageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Initial commit
     }
 }
 
