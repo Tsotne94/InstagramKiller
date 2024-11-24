cf90e1d259b194e8d7a3e0a174cf03e3ade17bb7
Sunny <116433775+SunnyDoe@users.noreply.github.com>
2024-11-24 20:54:38 +0400

    feat: add structures for handling notifications API response
    Add RootResponse and NotificationSections for parsing API response structure.
    
    Introduce NotificationJSONItem for transforming API JSON items.

--- InstsagramKiller/InstsagramKiller/MainAppModule/Scenes/NotificationsViewController/NotifcationTableView/Notification.swift
+++ InstsagramKiller/InstsagramKiller/MainAppModule/Scenes/NotificationsViewController/NotifcationTableView/Notification.swift
@@ -10,10 +10,28 @@ struct LikeNotification {
     let notifications: [NotificationItem]
 }
 
-struct NotificationItem {
+struct NotificationItem: Codable{
     let profileImage: String
     let username: String
     let action: String
     let timestamp: String
     let postImage: String?
+}
+
+struct RootResponse: Codable {
+    let data: NotificationSections
+}
+
+struct NotificationJSONItem: Codable {
+    let action: String
+    let profile_picture: String
+    let usernames: [String]
+    let time_ago: String
+}
+
+struct NotificationSections: Codable {
+    let new: [NotificationJSONItem]
+    let today: [NotificationJSONItem]
+    let this_week: [NotificationJSONItem]
+    let this_month: [NotificationJSONItem]
 }
