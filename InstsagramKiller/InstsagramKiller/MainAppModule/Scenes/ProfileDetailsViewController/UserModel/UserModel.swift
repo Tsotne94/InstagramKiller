f53095f571e0a82ac347c08107294e6511ac11be
Giorgi Kalaladze <giokakaladze4@gmail.com>
2024-11-25 01:38:20 +0400

    feature:Details Page View Model Setup

---
+++ InstsagramKiller/InstsagramKiller/MainAppModule/Scenes/ProfileDetailsViewController/UserModel/UserModel.swift
@@ -0,0 +1,41 @@
+//
+//  UserModel.swift
+//  InstsagramKiller
+//
+//  Created by Gio Kakaladze on 25.11.24.
+//
+import Foundation
+
+struct UserResponse: Codable {
+    let data: UserModel
+}
+
+struct UserModel: Codable {
+    let profilePicture: URL
+    let username: String
+    let fullName: String
+    let description: String
+    let counts: Counts
+    let photos: [URL]
+    let meta: Meta
+
+    struct Counts: Codable {
+        let posts: Int
+        let followers: Int
+        let following: Int
+    }
+
+    struct Meta: Codable {
+        let code: Int
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case profilePicture = "profile_picture"
+        case username
+        case fullName = "full_name"
+        case description
+        case counts
+        case photos
+        case meta
+    }
+}
